77c5238058b5cbbc2efa319bb478329518c9202c
Franwernke <franwernke@usp.br>
2022-06-07 11:40:00 -0300

    Create tables with just the basic information

--- setup.sql
+++ setup.sql
@@ -1,12 +1,38 @@
 CREATE IF NOT EXISTS TABLE Usuario {
-  int carteira,
-  boolean privacidade_do_perfil,
-  varchar(256) cpf,
-  varchar(256) nome,
-  varchar(256) email
+  carteira double precision NOT NULL,
+  privacidade_do_perfil boolean NOT NULL,
+  cpf varchar(256) NOT NULL,
+  nome varchar(256) NOT NULL,
+  email varchar(256) NOT NULL
 }
 
 CREATE IF NOT EXISTS TABLE Apostas {
-  int valor,
-  varchar(256)
+  valor double precision NOT NULL,
+  tipo varchar(256) NOT NULL,
+  lucro_ou_perda varchar(256) NOT NULL
+}
+
+CREATE IF NOT EXISTS TABLE Jogo {
+  "local" varchar(256) NOT NULL,
+  data_horario TIMESTAMP NOT NULL,
+  timeA varchar(256) NOT NULL,
+  timeB varchar(256) NOT NULL,
+  campeonato varchar(256)
+}
+
+CREATE IF NOT EXISTS TABLE "Time" {
+  nome varchar(256) NOT NULL,
+  numero_de_resenhas int8
+}
+
+CREATE IF NOT EXISTS TABLE Jogador {
+  nome varchar(256) NOT NULL,
+  data_de_nascimeno DATE NOT NULL,
+  local_de_nascimento varchar(256) NOT NULL,
+  numero_de_resenhas int8
+}
+
+CREATE IF NOT EXISTS TABLE Deposito {
+  codigo int8,
+  valor double precision
 }
