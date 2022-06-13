563d5975a03b05c9924a81ca58ea2fe1800dd420
Franwernke <franwernke@usp.br>
2022-06-13 13:46:40 -0300

    Create all tables (fix sintax and test)

--- BDSchemas/setup.sql
+++ BDSchemas/setup.sql
@@ -1,38 +1,38 @@
-CREATE IF NOT EXISTS TABLE Usuario {
+CREATE TABLE IF NOT EXISTS Usuario (
   carteira double precision NOT NULL,
   privacidade_do_perfil boolean NOT NULL,
   cpf varchar(256) NOT NULL,
   nome varchar(256) NOT NULL,
   email varchar(256) NOT NULL
-}
+);
 
-CREATE IF NOT EXISTS TABLE Apostas {
+CREATE TABLE IF NOT EXISTS Apostas (
   valor double precision NOT NULL,
   tipo varchar(256) NOT NULL,
   lucro_ou_perda varchar(256) NOT NULL
-}
+);
 
-CREATE IF NOT EXISTS TABLE Jogo {
+CREATE TABLE IF NOT EXISTS Jogo (
   "local" varchar(256) NOT NULL,
   data_horario TIMESTAMP NOT NULL,
   timeA varchar(256) NOT NULL,
   timeB varchar(256) NOT NULL,
   campeonato varchar(256)
-}
+);
 
-CREATE IF NOT EXISTS TABLE "Time" {
+CREATE TABLE IF NOT EXISTS "time" (
   nome varchar(256) NOT NULL,
   numero_de_resenhas int8
-}
+);
 
-CREATE IF NOT EXISTS TABLE Jogador {
+CREATE TABLE IF NOT EXISTS Jogador (
   nome varchar(256) NOT NULL,
   data_de_nascimeno DATE NOT NULL,
   local_de_nascimento varchar(256) NOT NULL,
   numero_de_resenhas int8
-}
+);
 
-CREATE IF NOT EXISTS TABLE Deposito {
+CREATE TABLE IF NOT EXISTS Deposito (
   codigo int8,
   valor double precision
-}
+);
