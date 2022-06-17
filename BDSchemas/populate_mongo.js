e675375e9aeeae656f0ab96a6746f692d9a1cfd9
danlawand <daniel.lawand@usp.br>
2022-06-17 19:58:28 -0300

    [MongoDB] queries update

--- BDSchemas/populate_mongo.js
+++ BDSchemas/populate_mongo.js
@@ -384,7 +384,7 @@ db.times.insertMany([
 ]);
 
 // ---------------------------------------------------------------------------------------------------
-db.jogador.insertMany([
+db.jogadores.insertMany([
     {
         "id":160,
         "info":
@@ -57919,7 +57919,7 @@ db.jogador.insertMany([
 ]);
 
 // ---------------------------------------------------------------------------------------------------
-db.jogo.insertMany([
+db.jogos.insertMany([
     {
         "id": 100,
         "info": {
@@ -67042,7 +67042,7 @@ db.jogo.insertMany([
     }
 ]);
 
-db.usuario.insertMany([
+db.usuarios.insertMany([
     {
       "id": 1,
       "carteira": 434,
@@ -67078,16 +67078,22 @@ db.usuario.insertMany([
 // Queries
 
 // Devolver todas as partidas com mais de 5 gols
-db.jogo.aggregate([{ $group : { _id: {matchId: "$id", awayTeam: "$info.awayTeam", homeTeam: "$info.homeTeam"}, golsPartida: { $max : {$sum: [{$toInt: "$info.goalsAwayTeam"},{$toInt: "$info.goalsHomeTeam"}]}}}}, {$sort: {golsPartida: -1}}, {$match: {golsPartida: {$gte: 5}}}]);
+db.jogos.aggregate([{ $group : { _id: {matchId: "$id", awayTeam: "$info.awayTeam", homeTeam: "$info.homeTeam"}, golsPartida: { $max : {$sum: [{$toInt: "$info.goalsAwayTeam"},{$toInt: "$info.goalsHomeTeam"}]}}}}, {$sort: {golsPartida: -1}}, {$match: {golsPartida: {$gte: 5}}}]);
 
 // Devolver os 5 times com os melhores saldo de gols
 db.times.aggregate({ $group : { _id: "$name", gD: {$max: "$leagues.goalsDiff"}}}, {$sort: {gD:-1}}, {$limit: 5});
 
 // Devolver o usuário com maior lucro
-db.usuario.aggregate({ $group : { _id: "$nome", lucro: { $max : "$lucro" }}}, {$sort: {lucro:-1}}, {$limit: 1});
+db.usuarios.aggregate({ $group : { _id: "$nome", lucro: { $max : "$lucro" }}}, {$sort: {lucro:-1}}, {$limit: 1});
 
 // Devolver o usuário com mais apostas
-db.usuario.aggregate({ $group : { _id: "$nome", quantidadeApostas: { $max : {$sum: ["$apostasVencedoras", "$apostasPerdedoras"]}}}}, {$sort: {quantidadeApostas: -1}}, {$limit: 1});
+db.usuarios.aggregate({ $group : { _id: "$nome", quantidadeApostas: { $max : {$sum: ["$apostasVencedoras", "$apostasPerdedoras"]}}}}, {$sort: {quantidadeApostas: -1}}, {$limit: 1});
 
 // Devolver jogador com mais cartões
-db.jogador.aggregate([{$unwind: "$statistics"},{$group: {_id: {id: "$id", firstName: "$info.firstname", lastName: "$info.lastname" }, cards: { $max : {$sum: ["$statistics.cards.red", "$statistics.cards.yellow", "$statistics.cards.yellowred"]}}}}, {$sort: {cards: -1}}, {$limit: 5}]);
+db.jogadores.aggregate([{$unwind: "$statistics"},{$group: {_id: {id: "$id", firstName: "$info.firstname", lastName: "$info.lastname" }, cards: { $max : {$sum: ["$statistics.cards.red", "$statistics.cards.yellow", "$statistics.cards.yellowred"]}}}}, {$sort: {cards: -1}}, {$limit: 5}]);
+
+// Classificação dos times que terminam em 'ham'
+db.times.find({"name":{$regex: /.*(ham)$/}}, {_id: 0, name: 1, leagues:1})
+
+// Liste os times que terminaram com mais de 60 pontos 
+db.times.aggregate({$group: {_id: "$name", points : {$max : "$leagues.points"}}}, {$match: {points: {$gte: 60}}}, {$sort: {points: -1}})
